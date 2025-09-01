1ee80c5b8bf7be858fafdfb7900863d1488c86f7
dina ali <alidina275@gamil.com>
2025-09-01 11:45:26 +0200

    initialize project added empty home screen added colors and app routes

---
+++ lib/ui/home/home_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:islami_app/utils/app_colors.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
---
+++ lib/utils/app_colors.dart
@@ -0,0 +1,14 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const primaryColor = Color(0xFFE2BE7F);
+  static const black = Color(0xFF202020);
+  static const white = Color(0xFFFFFFFF);
+  static const blackPg = Color(0x99202020);
+  static const gray = Color(0xFF707070);
+  static const offWhite = Color(0xFFFFF5E3);
+  static const brown = Color(0xFF946107);
+  static const transparent = Colors.transparent;
+}
