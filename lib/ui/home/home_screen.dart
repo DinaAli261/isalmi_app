3169d40d358b8b0fa75298e59c4f475b50a24720
DinaAli261 <alidina275@gmail.com>
2025-09-04 20:52:44 +0300

    Revert "Develop"

--- lib/ui/home/home_screen.dart
+++ lib/ui/home/home_screen.dart
@@ -1,23 +1,11 @@
 import 'package:flutter/material.dart';
-
-import '../intro/intro_screen.dart';
+import 'package:islami_app/utils/app_colors.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
-  void _onBackToIntro(BuildContext context) {
-    Navigator.of(
-      context,
-    ).pushReplacement(MaterialPageRoute(builder: (_) => const IntroScreen()));
-  }
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: ElevatedButton(
-        onPressed: () => _onBackToIntro(context),
-        child: const Text('Back to Introduction'),
-      ),
-    );
+    return Scaffold();
   }
 }
