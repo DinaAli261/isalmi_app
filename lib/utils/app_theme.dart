c5d4167d583d7365876cbe974ecbd85170cca233
dina ali <alidina275@gamil.com>
2025-09-04 14:47:55 +0200

    + added bottom navigation bar +added background images

---
+++ lib/utils/app_theme.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:islami_app/utils/app_colors.dart';
+
+class AppTheme {
+  static ThemeData darkTheme = ThemeData(
+    scaffoldBackgroundColor: AppColors.transparent,
+    bottomNavigationBarTheme: BottomNavigationBarThemeData(
+      type: BottomNavigationBarType.fixed,
+      backgroundColor: AppColors.primaryColor,
+      unselectedItemColor: AppColors.black,
+      selectedItemColor: AppColors.white,
+      showSelectedLabels: true,
+      showUnselectedLabels: false,
+    ),
+  );
+}
